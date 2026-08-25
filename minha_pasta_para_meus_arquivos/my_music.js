00e047c82c65db3a3522ed5afe109d21cc9f4596
Taquisuno <taylortaqui@gmail.com>
2026-08-25 14:45:36 -0300

    Implement audio oscillator with multiple wave types
    Added an audio context and oscillator function to play different wave types sequentially.

--- minha_pasta_para_meus_arquivos/my_music.js
+++ minha_pasta_para_meus_arquivos/my_music.js
@@ -1,1 +1,21 @@
+const audio = new AudioContext();
+let repeticoes = [0, 0, 0, 0];
 
+function delayHelp( ms){
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function tocar(){
+	const oscilador = audio.createOscillator();
+	oscilador.frequency.value = 450;
+	oscilador.type = "sine";
+	oscilador.connect(audio.destination);
+	oscilador.start();
+	await delayHelp(2000);
+	oscilador.type = "square";
+	await delayHelp(2000);
+	oscilador.type = "sawtooth";
+	await delayHelp(2000);
+	oscilador.type = "triangle";
+	oscilador.stop();
+}

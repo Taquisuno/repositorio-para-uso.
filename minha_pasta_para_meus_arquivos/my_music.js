7006ee90648fbf11a20c99176498fa93e7696e5e
Taquisuno <taylortaqui@gmail.com>
2026-08-25 14:55:04 -0300

    Refactor tocar function to use multiple oscillators

--- minha_pasta_para_meus_arquivos/my_music.js
+++ minha_pasta_para_meus_arquivos/my_music.js
@@ -6,17 +6,20 @@ function delayHelp( ms){
 }
 
 async function tocar(){
-	const oscilador = audio.createOscillator();
-	oscilador.frequency.value = 450;
-	oscilador.type = "sine";
-	oscilador.connect(audio.destination);
-	oscilador.start();
-	await delayHelp(2000);
-	oscilador.type = "square";
-	await delayHelp(2000);
-	oscilador.type = "sawtooth";
-	await delayHelp(2000);
-	oscilador.type = "triangle";
-	await delayHelp(2000);
-	oscilador.stop();
+let audio = new AudioContext();
+let repeticoes = [0, 0, 0, 0];
+
+function delayHelp( ms){
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function tocar(){
+	let oscilador = [audio.createOscillator(), audio.createOscillator(), audio.createOscillator(), audio.createOscillator()];
+	oscilador[0].frequency.value = 200;
+	oscilador[0].type = "triangle";
+	oscilador[0].connect(audio.destination);
+	oscilador[0].start();
+	await delayHelp(50);
+	oscilador[0].stop();
+}
 }

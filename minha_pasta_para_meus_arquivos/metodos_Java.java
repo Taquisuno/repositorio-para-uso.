24f44011647e9e53dc76b4e55c8d25299e5cfdb1
Taquisuno <taylortaqui@gmail.com>
2026-09-02 11:27:59 -0300

    Refactor Java methods and update main class structure

--- minha_pasta_para_meus_arquivos/metodos_Java.java
+++ minha_pasta_para_meus_arquivos/metodos_Java.java
@@ -1,14 +1,39 @@
 import java.util.Scanner;
-public class main{
+public class Main{
 	static Scanner input = new Scanner(System.in); 
 	public static void main(String[] args){
-		soma();
+	    String cumprimento = imprimirNome(input.nextLine());
+	    System.out.println(cumprimento + "\n");
+		double x = soma();
+		System.out.println(x + "\n");
+		double y = multiplicacao();
+		System.out.println(y + "\n");
+		boolean idadeV = verificacao(input.nextInt());
+		System.out.println(idadeV);
 	}
-    public static int soma(int a, int b){
-        a = input.nextInt()
-		input.nextLine();
-		b = input.nextInt();
-		input.nextLine();
-		return a + b;
+	static double a;
+	static double b;
+    public static double soma(){
+       a = input.nextDouble();
+       input.nextLine();
+       b = input.nextDouble();
+       input.nextLine();
+       return a + b;
+    }
+    public static double multiplicacao(){
+       a = input.nextDouble();
+       input.nextLine();
+       b = input.nextDouble();
+       input.nextLine();
+       return a * b;
+    }
+    public static String imprimirNome(String nome){
+        return "olá " + nome;
+    }
+    public static boolean verificacao(int idade){
+        if(idade >= 18){
+            return true;
+        }
+        return false;
     }
 }

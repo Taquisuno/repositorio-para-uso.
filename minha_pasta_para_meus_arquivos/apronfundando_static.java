9c033ef7bd7592b2dcd817ba2a25a5c19007f1f8
Taquisuno <taylortaqui@gmail.com>
2026-09-23 14:00:54 -0300

    Fix class declaration and method usage

--- minha_pasta_para_meus_arquivos/apronfundando_static.java
+++ minha_pasta_para_meus_arquivos/apronfundando_static.java
@@ -1,5 +1,17 @@
-public class{
+class Metodo{
+  String nome_MT;
+  static String nome_func = "testar coisas";
+
+  public Metodo(String nome_MT){
+    this.nome_MT = nome_MT;
+  }
+  void Teste_de_metodo(){
+    System.out.println(nome_MT);
+  }
+}
+public class Main{
   public static void main(String[] args){
-    
+    Metedo mt = new Metodo("tester de metodos");
+    mt.Teste_de_metodo();
   }
 }
